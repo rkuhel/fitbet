95f359698c1d09d65fda9a71baca64e2a0d2b3b1
Ryan Kuhel <ryan.kuhel@gmail.com>
2013-10-16 22:16:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FitbetApp::Application.config.secret_key_base = 'b06c0109bf617c579a949f60b05866a002f2abebfa4d1d63e6301e9760110a07cb480260475d826f42a21072b3dc6c7dc3af1f91591b9ceec594eb92fa2482ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FitbetApp::Application.config.session_store :cookie_store, key: '_fitbet_app_session'
